987fc963301536d90525ca01aa517bc96a3fb4df
CI-Starly.Chen <starly.chen@cloud-interactive.com>
2019-08-19 13:40:32 +0800

    Initial Commit

---
+++ RedSo_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedSo_Project
+//
+//  Created by CI-Starly.Chen on 2019/8/19.
+//  Copyright © 2019 CI-Starly.Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
